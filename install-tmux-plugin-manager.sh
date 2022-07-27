17c04f4961906b84721e8ac825c606b71e4f7a55
Kenji <kenjimartinushiro@gmail.com>
2022-07-27 15:12:48 -0300

    Add bind to set layout

--- install-tmux-plugin-manager.sh
+++ install-tmux-plugin-manager.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source ~/.tmux.conf
 rm ~/.tmux.conf
-ln -s ~/tmux-config/.tmux.conf ~/.tmux.conf
+ln -s ./tmux.conf ~/.tmux.conf
+tmux source ~/.tmux.conf
